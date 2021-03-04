4458b0ccd6d9f72bcbc3a569e1367f295aa68148
canto9112 <cantoweb112@gmail.com>
2021-03-04 17:23:54 +0300

    Download books from tululu.org

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from pathlib import Path
 
 
-def fetch_book():
-    url = 'https://tululu.org/txt.php?id=32168'
+def fetch_book(url, filename, path):
     response = requests.get(url, verify=False)
     response.raise_for_status()
-    filename = 'first_book.txt'
-    with open(filename, 'wb') as file:
+    with open(f'{path}/{filename}', 'wb') as file:
         file.write(response.content)
 
 
 if __name__ == "__main__":
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-    fetch_book()
+
+    path = 'books'
+    Path(path).mkdir(parents=True, exist_ok=True)
+    id = 0
+
+    for book in range(1, 11):
+        id += 1
+        print(id)
+        filename = f'id{id}.txt'
+        url = f'https://tululu.org/txt.php?id={id}'
+        fetch_book(url, filename, path)
