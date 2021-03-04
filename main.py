a20fe4cfc37628809b08105815892904ae410b7b
canto9112 <cantoweb112@gmail.com>
2021-03-04 17:55:27 +0300

    New function check_for_redirect

--- main.py
+++ main.py
@@ -3,11 +3,22 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from pathlib import Path
 
 
-def fetch_book(url, filename, path):
+def fetch_book_url(url):
     response = requests.get(url, verify=False)
     response.raise_for_status()
+    return response
+
+
+def save_book(filename, path, response):
     with open(f'{path}/{filename}', 'wb') as file:
         file.write(response.content)
+
+
+def check_for_redirect(response):
+    if response.history:
+        raise requests.HTTPError
+    else:
+        pass
 
 
 if __name__ == "__main__":
@@ -15,11 +26,18 @@ if __name__ == "__main__":
 
     path = 'books'
     Path(path).mkdir(parents=True, exist_ok=True)
-    id = 0
 
+    id = 0
     for book in range(1, 11):
         id += 1
-        print(id)
         filename = f'id{id}.txt'
         url = f'https://tululu.org/txt.php?id={id}'
-        fetch_book(url, filename, path)
+
+        response = fetch_book_url(url)
+        try:
+            check_for_redirect(response)
+            save_book(filename, path, response)
+        except requests.HTTPError:
+            print('requests.HTTPError')
+            continue
+
