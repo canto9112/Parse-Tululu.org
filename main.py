afbbd6324c7044dae59f196d8aff9dde267114fa
canto9112 <cantoweb112@gmail.com>
2021-03-06 11:42:30 +0300

    Новая функция скачивания книг

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from pathlib import Path
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
+import os
 
 
 def fetch_book_url(url):
@@ -9,9 +12,13 @@ def fetch_book_url(url):
     return response
 
 
-def save_book(filename, path, response):
-    with open(f'{path}/{filename}', 'wb') as file:
+def save_book(filename, response, folder='books'):
+    Path(folder).mkdir(parents=True, exist_ok=True)
+    sanitized_fname = sanitize_filename(filename)
+    path = os.path.join(folder, sanitized_fname)
+    with open(f'{path}.txt', 'wb') as file:
         file.write(response.content)
+    return f'{path}.txt'
 
 
 def check_for_redirect(response):
@@ -21,22 +28,37 @@ def check_for_redirect(response):
         pass
 
 
+def get_book_title(url):
+    response = requests.get(url, verify=False)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    title_tag = soup.find('h1').text
+
+    if len(title_tag.split('::')) == 2:
+        title = title_tag.split('::')[0].strip()
+        author = title_tag.split('::')[1].strip()
+    else:
+        title = title_tag.split('::')[0].strip()
+        author = 'Нет автора'
+    return title, author
+
+
 if __name__ == "__main__":
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-
-    path = 'books'
-    Path(path).mkdir(parents=True, exist_ok=True)
 
     id = 0
     for book in range(1, 11):
         id += 1
-        filename = f'id{id}.txt'
-        url = f'https://tululu.org/txt.php?id={id}'
+        txt_url = f'https://tululu.org/txt.php?id={id}'
+        book_url = f'https://tululu.org/b{id}/'
 
-        response = fetch_book_url(url)
+        filename, author = get_book_title(book_url)
+
+        response = fetch_book_url(txt_url)
         try:
             check_for_redirect(response)
-            save_book(filename, path, response)
+            filepath = save_book(filename, response, folder='txt')
+            print(filepath)
         except requests.HTTPError:
             print('requests.HTTPError')
             continue
