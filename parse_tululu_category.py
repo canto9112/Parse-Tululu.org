b44957b54d152ebd25d024b1fd6bbe154d904099
canto9112 <cantoweb112@gmail.com>
2021-03-31 23:48:54 +0300

    Новый файл - парсим категории

---
+++ parse_tululu_category.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import unquote, urljoin, urlsplit
+from pprint import pprint
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from pathlib import Path
+import os
+from pathvalidate import sanitize_filename
+
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+def get_book_urls(url):
+    response = requests.get(url, verify=False)
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    all_book_url_tag = soup.find_all('table', class_='d_book')
+    urls = []
+    for tag_url in all_book_url_tag:
+        book_url_tag = tag_url.find('a')['href']
+        book_url = urljoin(url, book_url_tag)
+        urls.append(book_url)
+    return urls
+
+
+def fetch_all_page_urls():
+    all_urls = []
+    for page in range(1,2):
+        url = f'https://tululu.org/l55/{page}/'
+        all_urls.extend(get_book_urls(url))
+    return all_urls
+
+
