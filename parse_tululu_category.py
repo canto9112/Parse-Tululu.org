a4e0a9051ea662477d3d1526b544049b80743bc1
canto9112 <cantoweb112@gmail.com>
2021-04-01 20:21:20 +0300

    Применил селектор в функции получения ссылок на книги

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -16,21 +16,20 @@ def get_book_urls(url):
     response.raise_for_status()
 
     soup = BeautifulSoup(response.text, 'lxml')
-
-    all_book_url_tag = soup.find_all('table', class_='d_book')
+    selector_url = '.d_book a'
+    url_book_tags = soup.select(selector_url)
     urls = []
-    for tag_url in all_book_url_tag:
-        book_url_tag = tag_url.find('a')['href']
-        book_url = urljoin(url, book_url_tag)
-        urls.append(book_url)
+    for tag_url in url_book_tags:
+        if tag_url.get('href').endswith('/'):
+            book_url_tag = tag_url.get('href')
+            book_url = urljoin(url, book_url_tag)
+            urls.append(book_url)
     return urls
 
 
 def fetch_all_page_urls():
     all_urls = []
-    for page in range(1,2):
+    for page in range(1, 2):
         url = f'https://tululu.org/l55/{page}/'
         all_urls.extend(get_book_urls(url))
     return all_urls
-
-
