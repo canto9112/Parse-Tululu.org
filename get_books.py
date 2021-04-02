73a0627e87505f38db9c2a8d9643c797271fdd41
canto9112 <cantoweb112@gmail.com>
2021-04-02 18:54:39 +0300

    Новый файл - get_books.py

---
+++ get_books.py
@@ -0,0 +1,60 @@
+from urllib.parse import unquote, urljoin, urlsplit
+import os
+import requests
+from pathlib import Path
+from pathvalidate import sanitize_filename
+
+
+def fetch_title_and_author(soup):
+    tag = soup.find('h1').text
+
+    if len(tag.split('::')) == 2:
+        title = tag.split('::')[0].strip()
+        author = tag.split('::')[1].strip()
+    else:
+        title = tag.split('::')[0].strip()
+        author = 'Нет автора'
+    return title, author
+
+
+def fetch_book_image_url(url, soup):
+    selector_image_url = '.bookimage img'
+    image_url_tag = soup.select_one(selector_image_url).get('src')
+    image_url = urljoin(url, image_url_tag)
+    return image_url
+
+
+def download_book_cover(url, folder='img'):
+    Path(folder).mkdir(parents=True, exist_ok=True)
+
+    cover_path = urlsplit(url).path
+    _, imagename = os.path.split(cover_path)
+
+    path = os.path.join(folder, sanitize_filename(unquote(imagename)))
+    response = requests.get(url, verify=False)
+    response.raise_for_status()
+    with open(path, 'wb') as file:
+        file.write(response.content)
+    return path
+
+
+def downoload_comment(soup):
+    selector_comment = '.texts .black'
+    new_comment_texts = soup.select(selector_comment)
+
+    comments_text = []
+
+    for comment in new_comment_texts:
+        comments_text.append(comment.get_text())
+    return comments_text
+
+
+def get_genres(soup):
+    selector_genres = '.d_book'
+    new_genres_tag = soup.select(selector_genres)
+    genres = []
+    for tag in new_genres_tag:
+        if tag.get_text().startswith('Жанр книги:'):
+            genr = tag.get_text().replace('Жанр книги:', '')
+            genres.append(genr.lstrip())
+    return genres
--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import parse_tululu_category
 import json
 from pprint import pprint
 import config
+import get_books
 
 
 def fetch_url_response(url, id):
@@ -35,39 +36,6 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
-def fetch_title_and_author(soup):
-    tag = soup.find('h1').text
-
-    if len(tag.split('::')) == 2:
-        title = tag.split('::')[0].strip()
-        author = tag.split('::')[1].strip()
-    else:
-        title = tag.split('::')[0].strip()
-        author = 'Нет автора'
-    return title, author
-
-
-def fetch_book_image_url(url, soup):
-    selector_image_url = '.bookimage img'
-    image_url_tag = soup.select_one(selector_image_url).get('src')
-    image_url = urljoin(url, image_url_tag)
-    return image_url
-
-
-def download_book_cover(url, folder='img'):
-    Path(folder).mkdir(parents=True, exist_ok=True)
-
-    cover_path = urlsplit(url).path
-    _, imagename = os.path.split(cover_path)
-
-    path = os.path.join(folder, sanitize_filename(unquote(imagename)))
-    response = requests.get(url, verify=False)
-    response.raise_for_status()
-    with open(path, 'wb') as file:
-        file.write(response.content)
-    return path
-
-
 def save_book(filename, response, folder='books'):
     Path(folder).mkdir(parents=True, exist_ok=True)
     path = os.path.join(folder, sanitize_filename(filename))
@@ -76,35 +44,13 @@ def save_book(filename, response, folder='books'):
     return path
 
 
-def downoload_comment(soup):
-    selector_comment = '.texts .black'
-    new_comment_texts = soup.select(selector_comment)
-
-    comments_text = []
-
-    for comment in new_comment_texts:
-        comments_text.append(comment.get_text())
-    return comments_text
-
-
-def get_genres(soup):
-    selector_genres = '.d_book'
-    new_genres_tag = soup.select(selector_genres)
-    genres = []
-    for tag in new_genres_tag:
-        if tag.get_text().startswith('Жанр книги:'):
-            genr = tag.get_text().replace('Жанр книги:', '')
-            genres.append(genr.lstrip())
-    return genres
-
-
 def parse_book_page(book_url, index_url):
     book_page = {}
     soup = get_soup(book_url)
-    title, author = fetch_title_and_author(soup)
-    image_url = fetch_book_image_url(index_url, soup)
-    genres = get_genres(soup)
-    comments_text = downoload_comment(soup)
+    title, author = get_books.fetch_title_and_author(soup)
+    image_url = get_books.fetch_book_image_url(index_url, soup)
+    genres = get_books.get_genres(soup)
+    comments_text = get_books.downoload_comment(soup)
     book_page.update({'title': title,
                       'author': author,
                       'image_link': image_url,
@@ -115,7 +61,6 @@ def parse_book_page(book_url, index_url):
 
 def main():
     start_id, end_id, dest_folder = config.get_arguments()
-    print(dest_folder)
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
     index_url = 'https://tululu.org/'
@@ -136,14 +81,14 @@ def main():
             check_for_redirect(url_response)
             book_page = parse_book_page(book_url, index_url)
             image_link = book_page['image_link']
-            img_src = download_book_cover(image_link)
+            img_src = get_books.download_book_cover(image_link)
             title = book_page['title']
             filename = f'{title}.txt'
             book_path = save_book(filename, url_response, folder='books')
             author = book_page['author']
             soup = get_soup(book_url)
-            comments = downoload_comment(soup)
-            genres = get_genres(soup)
+            comments = get_books.downoload_comment(soup)
+            genres = get_books.get_genres(soup)
             fantastic_books.append({'title': title,
                                     'author': author,
                                     'img_src': img_src,
