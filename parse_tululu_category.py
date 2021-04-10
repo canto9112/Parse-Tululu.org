867d53c9f1ae54e2f24fffca56e3fe14889b90d8
canto9112 <cantoweb112@gmail.com>
2021-04-10 10:32:07 +0300

    Добавил функцию получения последней номера последней страницы в категории

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -14,13 +14,10 @@ def get_book_urls(url):
 
     urls = []
     for tag_url in book_tags:
-
         if tag_url.get('href').endswith('/'):
             id_book = tag_url.get('href')
-
             if id_book.startswith('/b'):
                 book_url = urljoin(url, id_book)
-
                 urls.append(book_url)
     return set(urls)
 
@@ -31,3 +28,18 @@ def fetch_all_page_urls(start_page, end_page):
         url = f'https://tululu.org/l55/{page}/'
         all_urls.extend(get_book_urls(url))
     return all_urls
+
+
+def get_last_page_number():
+    url = 'https://tululu.org/l55/'
+    response = requests.get(url, verify=False)
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.text, 'lxml')
+    selector_page = '.center .npage'
+    page_tags = soup.select(selector_page)
+    for tag in page_tags:
+        last_number = tag.text
+    return last_number
+
+
