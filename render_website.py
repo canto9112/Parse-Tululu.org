89ea982996622a472b4f810e96bfa7b2af8a20fa
canto9112 <cantoweb112@gmail.com>
2021-04-17 20:59:10 +0300

    Разделил список книг по страницам

--- render_website.py
+++ render_website.py
@@ -4,6 +4,7 @@ from more_itertools import chunked
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 from pprint import pprint
+import os
 
 
 def get_template():
@@ -19,38 +20,41 @@ def get_books():
     with open('result/JSON.json', 'r') as my_file:
         file_json = my_file.read()
     books = json.loads(file_json)
-
-    return list(chunked(books, 2))
+    return list(chunked(books, 10))
 
 
 def get_rendered_page(template, books):
-    rendered_page = template.render(
-        books=books
-    )
-    return rendered_page
+    rendered_pages = []
+    print(books)
+    for page_number, page in enumerate(books, 1):
+        print(page_number, page)
+        rendered_page = template.render(books=page)
+        rendered_pages.append(rendered_page)
+
+    return rendered_pages
 
 
-def get_index_html(rendered_page):
-    with open('index.html', 'w', encoding="utf8") as file:
+def get_index_html(rendered_page, page_number):
+
+    with open(f'pages/index{page_number}.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
 
 def rebuild():
     template = get_template()
     books = get_books()
-    rendered_page = get_rendered_page(template, books)
-    get_index_html(rendered_page)
+    rendered_pages = get_rendered_page(template, books)
+    os.makedirs('pages')
+    for page_number, rendered_page in enumerate(rendered_pages, 1):
+        get_index_html(rendered_page, page_number)
+
     print("Site rebuilt")
 
 
 def main():
-
     rebuild()
-
     server = Server()
-
     server.watch('template.html', rebuild)
-
     server.serve(root='.')
 
 
